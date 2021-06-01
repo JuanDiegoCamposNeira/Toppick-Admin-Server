7f73189ab2fe067d54c4234476a2b51ed5d9c646
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2021-05-31 20:56:09 -0500

    node

--- Scripts/heder_links.js
+++ Scripts/heder_links.js
@@ -8,7 +8,7 @@ var botonPantallaActualizar = document.getElementById("PatnallaActualizar");
 var botonPantallaHisto = document.getElementById("PantallaHisto");
 var botonPantallaCierre = document.getElementById("PantallaCierre");
 
-botonPantallaPedidos.addEventListener("click", () => { window.location.replace('http://localhost:8080/pedidos'); });
-botonPantallaActualizar.addEventListener("click", () => { window.location.replace('http://localhost:8080/actualizar'); });
-botonPantallaHisto.addEventListener("click", () => { window.location.replace('http://localhost:8080/historial'); });
-botonPantallaCierre.addEventListener("click", () => { window.location.replace('http://localhost:8080/cierre_caja'); });
+botonPantallaPedidos.addEventListener("click", () => { window.location.replace('https://toppick-admin-server.herokuapp.com/pedidos'); });
+botonPantallaActualizar.addEventListener("click", () => { window.location.replace('https://toppick-admin-server.herokuapp.com/actualizar'); });
+botonPantallaHisto.addEventListener("click", () => { window.location.replace('https://toppick-admin-server.herokuapp.com/historial'); });
+botonPantallaCierre.addEventListener("click", () => { window.location.replace('https://toppick-admin-server.herokuapp.com/cierre_caja'); });
